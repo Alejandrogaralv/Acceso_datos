96bd589dd651aae7662e90144b0f4dffffddcc73
Alejandrogaralv <alejandrogaralv448@gmail.com>
2022-10-06 10:12:43 +0200

    Commit clase

--- Lanzar_excepciones/src/Main.java
+++ Lanzar_excepciones/src/Main.java
@@ -7,6 +7,11 @@ public class Main {
         diaSemana(5);
         diaSemana(6);
         diaSemana(7);
+        try{
+            diaSemanaThrows(1);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
     }
     public static void diaSemana (int num){
         try{
@@ -29,5 +34,21 @@ public class Main {
         }catch (Exception e){
             System.err.println(e.toString());
         }
+    }  public static void diaSemanaThrows (int num) throws Exception{
+            if(num==1){
+                throw new Exception("Hoy es lunes");
+            } else if (num==2) {
+                throw new Exception("Hoy es martes");
+            } else if (num==3) {
+                throw new Exception("Hoy es miércoles");
+            } else if (num==4) {
+                throw new Exception("Hoy es jueves");
+            } else if (num==5) {
+                throw new Exception("Hoy es viernes");
+            } else if (num==6) {
+                throw new Exception("Hoy es sábado");
+            } else if (num==7) {
+                throw new Exception("Hoy es domingo");
+            }
     }
 }
